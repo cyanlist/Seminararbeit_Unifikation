149a498a95319d8898a7d77b9d1a89bc91bb13c7
Cyanlist <cyanlist@outlook.com>
2025-04-27 16:25:17 +0200

    Finished Draft

--- src/main/java/com/github/geje1017/Main.java
+++ src/main/java/com/github/geje1017/Main.java
@@ -7,16 +7,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Variable X = new Variable("X");
-        Variable Y = new Variable("Y");
 
-        Term t3 = new Function("g", X);
-
-        Term t1 = new Function("f", t3, X);
-        Term t2 = new Function("g", X, new Constant("a"));
-
-        Equation eq = new Equation(t1, t2);
-
-        System.out.println(Unifier.unify(List.of(eq)));
     }
 }
